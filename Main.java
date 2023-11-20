a7ee51fe4f62ca8256bd8436a2b88a56ae1043d1
RabiuAmir <95980708+BabushkaBoi01@users.noreply.github.com>
2023-11-20 18:57:09 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Student cmpe411 = new Student();
+        cmpe411.display_All();
+        cmpe411.statistics();
+        //cmpe411.delete_Student("1235");
+        //cmpe411.add_Student(1008,"Eren Yeager",4.0,"01-01-0001","M");
+
+
+    }
+}
